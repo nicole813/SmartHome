a2026565af354948906b70721b3c537047e1befa
nicole813 <nicolechen7251@gmail.com>
2020-09-28 16:27:52 +0800

    修改為斷線才會發警報

--- ArduinoSmartHome/ArduinoSmartHome.ino
+++ ArduinoSmartHome/ArduinoSmartHome.ino
@@ -1,13 +1,21 @@
-void setup(){
+void setup()
+{
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(2, INPUT);
+  Serial.begin(9600);
 }
-
-void loop(){
+void loop()
+{
+if(digitalRead(2)==HIGH) {
   digitalWrite(12, HIGH);
   digitalWrite(11, LOW);
+  tone(7, 1000, 500);
   delay(500);
   digitalWrite(12, LOW);
   digitalWrite(11, HIGH);
+  tone(7, 800, 500);
   delay(500);
+}
 }
